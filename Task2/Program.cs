5b341bed400b070b8af6824ea7345794ba033a1a
Vladislav <vhasovnikov@inbox.ru>
2023-01-11 23:49:48 +0500

    Написал код для каждой задач, добавил инфу и кнопки в файл readme

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int ValueInput()
+{
+    Console.Write("Введите число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+void SumElementNumber(int Number)
+{
+int sum = 0;
+while (Number > 1)
+{
+    sum += Number % 10;
+    Number /= 10;
+}
+Console.WriteLine(sum);
+}
+int number = ValueInput();
+SumElementNumber(number);
